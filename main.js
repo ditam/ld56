490babf9c8366efd1e34a434984e64f42996ef06
ditam <ditam@users.noreply.github.com>
2024-10-07 19:55:29 +0200

    add audio handling, bg music and splash screen

--- main.js
+++ main.js
@@ -19,7 +19,41 @@ function start() {
   });
 }
 
+let songs, sounds;
+
 $(document).ready(function() {
   container = $('#main');
-  start();
+
+  songs = [
+    new Audio('bgmusic.mp3'),
+  ];
+
+  errorSound = new Audio('error.mp3');
+
+  sounds = [
+    errorSound,
+  ];
+
+  let audioLoadCount = 0;
+  $('#loadCountTotal').text(songs.length + sounds.length);
+  function countWhenLoaded(audioElement) {
+    audioElement.addEventListener('canplaythrough', function() {
+      audioLoadCount++;
+      $('#loadCount').text(audioLoadCount);
+    }, false);
+  }
+
+  $('#splash').on('click', function() {
+    $('#splash').remove();
+    songs[0].play();
+    songs[0].addEventListener('ended', function() {
+      this.currentTime = 0;
+      this.play();
+    }, false);
+
+    start();
+  });
+
+  songs.forEach(countWhenLoaded);
+  sounds.forEach(countWhenLoaded);
 });
